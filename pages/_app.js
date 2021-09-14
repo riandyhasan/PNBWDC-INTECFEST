922cec1d69a3cda88b6745d8ab0d28c7ff81452b
riandyhasan <riandyhsn@gmail.com>
2021-09-14 17:07:37 +0800

    set landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  fonts: {
+    heading: "Roboto, sans-serif",
+    body: "Roboto, sans-serif",
+  },
+  colors: {
+    brand: {
+      primary: "#BB371A",
+      secondary: "#EBA83A",
+      tertiary: "#FFF8D9",
+    },
+  },
+});
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Global />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
