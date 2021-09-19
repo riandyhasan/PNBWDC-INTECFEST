8f0e8dbcda22dfb5863405ade2e3580d4a1d688c
riandyhasan <riandyhsn@gmail.com>
2021-09-19 22:55:04 +0800

    add sticky components

--- components/Landing/CariDonor.js
+++ components/Landing/CariDonor.js
@@ -5,12 +5,12 @@ import PlasmaCard from "../PlasmaCard/PlasmaCard";
 
 export default function CariDonor({ ...props }) {
   return (
-    <Flex id="caridonor" padding="0rem 4rem 0rem 4rem" direction="column" {...props} overflow="visible">
+    <Flex id="caridonor" padding="0rem 4rem 0rem 4rem" direction="column" {...props}>
       <Heading color="brand.primary" fontSize="5xl" marginBottom="3rem">
         Donor Plasma di Dekatmu!
       </Heading>
-      <Flex alignItems="flex-start" justifyContent="space-around" gridGap={16} position="relative" overflow="visible" float="left" height="auto">
-        <Box padding="4rem" bg="brand.primary" borderRadius="13px" sx={{ zIndex: "100", display: "block", alignSelf: "flex-start", position: "-webkit-sticky", /* Safari */ position: "sticky", top: "0" }}>
+      <Flex alignItems="flex-start" justifyContent="space-around" gridGap={16}>
+        <Box padding="4rem" bg="brand.primary" borderRadius="13px" sx={{ position: "-webkit-sticky", /* Safari */ position: "sticky", top: "0" }}>
           <Map />
         </Box>
         <Grid templateColumns="repeat(1, 1fr)" gap={6}>
