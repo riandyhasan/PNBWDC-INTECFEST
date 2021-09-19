16421730517e61d97ce672a00db3ef75c943ea71
riandyhasan <riandyhsn@gmail.com>
2021-09-19 22:17:14 +0800

    Create Jumbotron.js

---
+++ components/TentangKami/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
+import { ArrowDownIcon } from "@chakra-ui/icons";
+import Link from "next/link";
+
+export default function TentangKamiJumbotron({ ...props }) {
+  return (
+    <Flex minH={{ base: "60vh", md: "80vh", lg: "88vh" }} minW="100vw" direction="column" alignItems="center" justifyContent="center" bgImg="url('/assets/img/FotoTentangKami.png')" bgRepeat="no-repeat" bgSize="cover" {...props}>
+      <Image src="assets/img/LogoPanjangSlogan.png" width={{ base: "90vw", md: "60vw", lg: "50vw" }} marginTop="auto" />
+      <Link href="#desc">
+        <ArrowDownIcon color="brand.tertiary" w={8} h={8} marginTop="auto" marginBottom="2rem" />
+      </Link>
+    </Flex>
+  );
+}
