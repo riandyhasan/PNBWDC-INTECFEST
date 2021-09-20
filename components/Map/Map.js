2e96fc105ad560c65ecd7e5f5d7622a62ba9c6ef
riandyhasan <riandyhsn@gmail.com>
2021-09-20 22:48:51 +0800

    Update Map.js

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import { Flex, Heading, Image, useMediaQuery } from "@chakra-ui/react";
+import ReactMapGL, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function Map({ ...props }) {
+  const [isLg] = useMediaQuery("(min-width: 990px)");
+
   const [viewport, setViewport] = useState({
-    width: "40vw",
     height: 400,
     latitude: 37.7577,
     longitude: -122.4376,
-    zoom: 8,
+    zoom: 14,
   });
 
-  return <ReactMapGL {...viewport} onViewportChange={(nextViewport) => setViewport(nextViewport)} {...props} />;
+  return (
+    <ReactMapGL
+      {...viewport}
+      width={isLg ? "40vw" : "70vw"}
+      mapboxApiAccessToken="pk.eyJ1IjoicmlhbmR5aGFzYW4iLCJhIjoiY2t0cmNwdTFiMTUwbzJvbDRjdG4zcHliciJ9.kWgnsrMNfY_h1t2BAmwaUQ"
+      mapStyle="mapbox://styles/riandyhasan/cktrdh2an04bf17qr9zx013qs"
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      {...props}
+    >
+      <Marker latitude={viewport.latitude} longitude={viewport.longitude}>
+        <Flex direction="column" alignItems="center">
+          <Image src="assets/svg/pin-merah.svg" width="1.4em" />
+          <Heading color="brand.primary" fontSize="sm">
+            Lokasimu
+          </Heading>
+        </Flex>
+      </Marker>
+    </ReactMapGL>
+  );
 }
