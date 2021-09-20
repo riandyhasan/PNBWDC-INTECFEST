65d577d8d401287ab686e42516a9f3d84a7154b8
riandyhasan <riandyhsn@gmail.com>
2021-09-20 22:49:02 +0800

    Update PlasmaCard.js

--- components/PlasmaCard/PlasmaCard.js
+++ components/PlasmaCard/PlasmaCard.js
@@ -1,38 +1,64 @@
 import React from "react";
-import { Box, Flex, Heading, Button, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Image, Text, useToast } from "@chakra-ui/react";
 import Link from "next/link";
 import copy from "copy-to-clipboard";
+import PlasmaDetails from "../PlasmaDetails/PlasmaDetails";
 
-export default function DonorCard({ ...props }) {
+export default function DonorCard({ onModalOpen, setModalWidth, setModalTitle, setModalContent, ...props }) {
+  const alamat = useToast();
+  const telp = useToast();
+
   return (
     <Box bg="brand.primary" display="flex" color="brand.tertiary" padding="2rem" justifyContent="space-between" {...props} maxW="500px" borderRadius="22px">
       <Box>
-        <Heading cursor="pointer" fontSize="xl">
+        <Heading
+          cursor="pointer"
+          fontSize="xl"
+          onClick={() => {
+            setModalTitle("Donor Banjarmasin");
+            setModalContent(<PlasmaDetails />);
+            setModalWidth("50vw");
+            onModalOpen();
+          }}
+        >
           Donor Banjarmasin
         </Heading>
         <Flex marginTop="1rem" alignItems="center" gridGap={4}>
-          <a href="https://github.com/" target="_blank">
+          <a
+            href="https://www.google.com/maps/place/Jl.+Bali,+Antasan+Besar,+Kec.+Banjarmasin+Tengah,+Kota+Banjarmasin,+Kalimantan+Selatan/data=!4m2!3m1!1s0x2de423c7123bcfb9:0x2ab95b256b2b9d99?sa=X&ved=2ahUKEwjo4bin2o3zAhXJH7cAHXZwDQgQ8gF6BAgPEAE"
+            target="_blank"
+          >
             <Image src="assets/svg/pin.svg" cursor="pointer" width="0.8em" marginLeft="0.4rem" />
           </a>
           <Text
             cursor="pointer"
             onClick={() => {
-              copy("Jl. Mana ya gatau gue");
-              alert(`Nomor telah berhasil dicopy!`);
+              copy("Jl. Bali No 32");
+              alamat({
+                title: "Alamat berhasil disalin!",
+                status: "success",
+                duration: 1000,
+                isClosable: true,
+              });
             }}
           >
-            Jl. Mana ya gatau gue
+            Jl. Bali No 32
           </Text>
         </Flex>
         <Flex marginTop="1rem" alignItems="center" gridGap={4}>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://api.whatsapp.com/send?phone=628783772731" target="_blank">
             <Image src="assets/svg/phone.svg" cursor="pointer" width="0.8em" marginLeft="0.4rem" />
           </a>
           <Text
             cursor="pointer"
             onClick={() => {
-              copy("Jl. Mana ya gatau gue");
-              alert(`Nomor telah berhasil dicopy!`);
+              copy("08783772731");
+              telp({
+                title: "Nomor berhasil disalin!",
+                status: "success",
+                duration: 1000,
+                isClosable: true,
+              });
             }}
           >
             087837727431
@@ -44,20 +70,18 @@ export default function DonorCard({ ...props }) {
           <Heading>33</Heading>
           <Image src="assets/svg/plasma-bag.svg" width="1.8em" marginLeft="0.4rem" />
         </Flex>
-        <Link href="/donor">
-          <Button
-            fontSize="1rem"
-            padding="1rem 1.3rem 1rem 1.3rem"
-            bg="brand.tertiary"
-            color="brand.primary"
-            _hover={{
-              bg: "brand.secondary",
-            }}
-          >
-            Lihat di Peta
-            <Image src="assets/svg/search-map.svg" width="0.8" marginLeft="0.4rem" />
-          </Button>
-        </Link>
+        <Button
+          fontSize="1rem"
+          padding="1rem 1.3rem 1rem 1.3rem"
+          bg="brand.tertiary"
+          color="brand.primary"
+          _hover={{
+            bg: "brand.secondary",
+          }}
+        >
+          Lihat di Peta
+          <Image src="assets/svg/search-map.svg" width="0.8" marginLeft="0.4rem" />
+        </Button>
       </Flex>
     </Box>
   );
