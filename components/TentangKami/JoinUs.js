9ab264fcfc0d506021bef0e2efd8e1baee81d023
riandyhasan <riandyhsn@gmail.com>
2021-09-20 22:49:15 +0800

    Update JoinUs.js

--- components/TentangKami/JoinUs.js
+++ components/TentangKami/JoinUs.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Box, Flex, Heading, Button, Image, Text } from "@chakra-ui/react";
 
-export default function BantuDonor({ ...props }) {
+export default function JoinUs({ ...props }) {
   return (
     <Flex bg="brand.primary" color="brand.tertiary" direction={{ base: "column", md: "row" }} padding="4rem" alignItems={{ base: "flex-start", md: "center" }} justifyContent={{ base: "flex-start", md: "space-between" }} {...props}>
       <Box>
-        <Heading>Tertarik Berkontribusi Melalui PlasmaHub?</Heading>
+        <Heading>Tertarik Berkontribusi Melalui PlasmaKu?</Heading>
         <Flex maxW="70vw">
           <Text textAlign="justify" marginTop={3}>
-            Anda ingin berkontribusi lebih di tengah pandemi? Ayo bergabung bersama tim PlasmaHub dan bantu lebih banyak orang dalam menghadapi COVID-19!
+            Anda ingin berkontribusi lebih di tengah pandemi? Ayo bergabung bersama tim PlasmaKu dan bantu lebih banyak orang dalam menghadapi COVID-19!
           </Text>
         </Flex>
       </Box>
