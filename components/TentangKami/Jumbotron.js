3a40cc21561b83115051e395b30ffe75c1ec94c5
riandyhasan <riandyhsn@gmail.com>
2021-09-20 22:49:18 +0800

    Update Jumbotron.js

--- components/TentangKami/Jumbotron.js
+++ components/TentangKami/Jumbotron.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Flex, Image } from "@chakra-ui/react";
 import { ArrowDownIcon } from "@chakra-ui/icons";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function TentangKamiJumbotron({ ...props }) {
   return (
     <Flex minH={{ base: "60vh", md: "80vh", lg: "88vh" }} w="100%" direction="column" alignItems="center" justifyContent="center" bgImg="url('/assets/img/FotoTentangKami.png')" bgRepeat="no-repeat" bgSize="cover" {...props}>
-      <Image src="assets/img/LogoPanjangSlogan.png" width={{ base: "90vw", md: "60vw", lg: "50vw" }} marginTop="auto" />
+      <Image src="assets/img/LogoPanjangSlogan.png" width={{ base: "90vw", md: "60vw", lg: "50vw" }} />
       <Link href="#desc">
-        <ArrowDownIcon color="brand.tertiary" w={8} h={8} marginTop="auto" marginBottom="2rem" />
+        <motion.div animate={{ y: [150, 165, 150] }} transition={{ type: "spring", stiffness: 100, ease: "linear", duration: 1, repeat: Infinity }}>
+          <ArrowDownIcon cursor="pointer" color="brand.tertiary" w={8} h={8} marginBottom="2rem" marginTop="auto" />
+        </motion.div>
       </Link>
     </Flex>
   );
